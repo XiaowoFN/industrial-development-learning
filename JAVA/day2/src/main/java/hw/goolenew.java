5b71afb5d9e16b45ee524354c6c535dc288b8b15
XiaowoFN <yuruichen4@gmail.com>
2026-08-18 11:50:39 +0800

    更换验证器

---
+++ JAVA/day2/src/main/java/hw/goolenew.java
@@ -0,0 +1,8 @@
+package hw;
+
+public class goolenew {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
