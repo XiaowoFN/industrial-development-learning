018fa9a3e41acd9aa7dd356d04882e77f13dd96d
XiaowoFN <yuruichen4@gmail.com>
2026-08-24 16:28:31 +0800

    类学习的开始（之前就是这里一直学不明白呜呜）

---
+++ JAVA/day3/src/main/java/hw/classL.java
@@ -0,0 +1,22 @@
+package hw;
+
+/*
+* ！！从这个文档开始说明和笔记会放在文件的上面！！
+*
+* 什么是对象？
+*
+*
+* */
+
+public class classL {
+    public static void main(String[] args) {
+//        System.out.println("hello world");
+
+    }
+}
+
+
+
+
+
+
